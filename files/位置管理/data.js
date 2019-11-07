f7fc528536461450fd3278829d10bb11c52de231
cotwo436 <52022102+cotwo436@users.noreply.github.com>
2019-11-07 20:04:48 +0800

    update
    1、增加实例管理

--- files/位置管理/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x,y],z,_(A,B,C,D,E,F,G,_(),H,_(I,J,K,L,M,_(N,O,P,Q),R,null,S,L,T,L,U,V,W,null,X,Y,Z,ba,bb,bc,bd,Y),be,_(),bf,_(),bg,_(bh,[])),bi,_(),bj,_());}; 
-var b="url",c="位置管理.html",d="generationDate",e=new Date(1573041464718.73),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="ByPass",n="StartUp",o="Login",p="Hint",q="Product",r="Slogan",s="UserName",t="SegmentIn",u="DefaultInstance",v="SearchWord",w="SearchPrevStep",x="SearchCurStep",y="SearchInMap",z="page",A="packageId",B="7673259e8f4c462ca62e2ca4b8e4a02c",C="type",D="Axure:Page",E="name",F="位置管理",G="notes",H="style",I="baseStyle",J="627587b6038d43cca051c114ac41ad32",K="pageAlignment",L="near",M="fill",N="fillType",O="solid",P="color",Q=0xFFFFFFFF,R="image",S="imageHorizontalAlignment",T="imageVerticalAlignment",U="imageRepeat",V="auto",W="favicon",X="sketchFactor",Y="0",Z="colorStyle",ba="appliedColor",bb="fontName",bc="Applied Font",bd="borderWidth",be="adaptiveStyles",bf="interactionMap",bg="diagram",bh="objects",bi="masters",bj="objectPaths";
-return _creator();
-})());
